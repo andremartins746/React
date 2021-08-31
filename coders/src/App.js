226abeb711ce24ea068c29c206ecb1c6359baf71
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-08-30 22:43:51 -0300

    new files of React

--- coders/src/App.js
+++ coders/src/App.js
@@ -1,21 +1,40 @@
+import React from "react"
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>< a href="/">André Martins Pereira</a></li>
+          <li><a href="/">Contatos</a></li>
+          <li>;)</li>
+        </ul>
       </header>
+
+      <main>
+       <h1>Hello Word Welcome</h1>
+       <section className="Content-Main">
+         <aside className="content1">
+           <h3><p>
+            Olá seja muito bem vindo(a) ao meu Porifólio, aqui eu apresento um pouco da minha trajetoria na área da Técnologia
+             apesar de ainda ela ser bem curta, tento aproveitar ao maximo tudo o que tenho aprendido ao longo deste tempo...
+           </p>
+           </h3>
+         </aside>
+         <aside className="content2">
+           <h3>hsdfjhsbdfjhsdfhsbfsdfbsdhfbsdfbsbf
+             hsdbfsjdhbfshbdfjsbdfjbsdfbsjdhbfjsdbf
+             sdfbsjdbfsdjbfsjdfsdbfsjkdbfksjdbfsjkdbf
+             sdfbsjdfbsdbfksdjbfksjdbfjksdbfkjsdfjksdfbjks
+             sdbfkjsdbfjksdbfjskdbfkjsbdfsjbdfsbdsjbdfkjsdbfkjs
+           </h3>
+         </aside>
+       </section>
+      </main>
+
+
     </div>
   );
 }
