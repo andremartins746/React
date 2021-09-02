1af0d53069e1531ed0e041e09748b4675d0b095f
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-09-02 17:50:10 -0300

    Update App.js

--- coders/src/App.js
+++ coders/src/App.js
@@ -1,40 +1,29 @@
-import React from "react"
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./App.css";
 
 
 function App() {
+  const [Repositorios, setRepositorios] = useState([])
+
+  useEffect(async () => {
+      const response = await fetch('https://api.github.com/users/andremartins746/repos');
+      const data = response.json()
+      setRepositorios(data)
+  },[])
+
+  console.log("Eai thiago Suave Mnao")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <ul>
-          <li>< a href="/">André Martins Pereira</a></li>
-          <li><a href="/">Contatos</a></li>
-          <li>;)</li>
-        </ul>
-      </header>
-
-      <main>
-       <h1>Hello Word Welcome</h1>
-       <section className="Content-Main">
-         <aside className="content1">
-           <h3><p>
-            Olá seja muito bem vindo(a) ao meu Porifólio, aqui eu apresento um pouco da minha trajetoria na área da Técnologia
-             apesar de ainda ela ser bem curta, tento aproveitar ao maximo tudo o que tenho aprendido ao longo deste tempo...
-           </p>
-           </h3>
-         </aside>
-         <aside className="content2">
-           <h3>hsdfjhsbdfjhsdfhsbfsdfbsdhfbsdfbsbf
-             hsdbfsjdhbfshbdfjsbdfjbsdfbsjdhbfjsdbf
-             sdfbsjdbfsdjbfsjdfsdbfsjkdbfksjdbfsjkdbf
-             sdfbsjdfbsdbfksdjbfksjdbfjksdbfkjsdfjksdfbjks
-             sdbfkjsdbfjksdbfjskdbfkjsbdfsjbdfsbdsjbdfkjsdbfkjs
-           </h3>
-         </aside>
-       </section>
-      </main>
-
-
+      <>
+      <ul>
+        {
+          Repositorios.map(repo => {
+            <li key={repo.id}>{repo.name}</li>
+          })
+        }
+      </ul>
+      </>
     </div>
   );
 }
