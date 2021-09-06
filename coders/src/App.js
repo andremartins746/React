1986e3c615c06ae8d9d38a44edc02d6066df165b
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-09-06 10:40:27 -0300

    Update App.js

--- coders/src/App.js
+++ coders/src/App.js
@@ -1,31 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
 
 
-function App() {
-  const [Repositorios, setRepositorios] = useState([])
+export default function App() {
 
-  useEffect(async () => {
-      const response = await fetch('https://api.github.com/users/andremartins746/repos');
-      const data = response.json()
-      setRepositorios(data)
-  },[])
+const typedata = async () => {
+    const response = await fetch("https://covid19.mathdro.id/api");
+    const Object = response.json()
+    console.log(Object)
+}
 
-  console.log("Eai thiago Suave Mnao")
-
+typedata()
+ 
   return (
     <div className="App">
       <>
-      <ul>
-        {
-          Repositorios.map(repo => {
-            <li key={repo.id}>{repo.name}</li>
-          })
-        }
-      </ul>
+        
       </>
     </div>
   );
 }
-
-export default App;
